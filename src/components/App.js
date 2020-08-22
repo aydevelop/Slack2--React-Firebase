1cd6a3025dd257d3a8c36e4270df43207970666d
aydevelop <ay.develop1@gmail.com>
2020-08-22 12:58:47 +0300

    Add app components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Grid } from 'semantic-ui-react'
 import './App.css'
+
+import ColorPanel from './ColorPanel/ColorPanel'
+import SidePanel from './SidePanel/SidePanel'
+import Messages from './Messages/Messages'
+import MetaPanel from './MetaPanel/MetaPanel'
 
 function App() {
   return (
-    <div className='App'>
-      <div>App</div>
+    <div className='app'>
+      <Grid columns='equal' style={{ background: '#eee' }}>
+        <ColorPanel />
+        <SidePanel />
+        <Grid.Column style={{ marginLeft: 320 }}>
+          <Messages />
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <MetaPanel />
+        </Grid.Column>
+      </Grid>
     </div>
   )
 }
