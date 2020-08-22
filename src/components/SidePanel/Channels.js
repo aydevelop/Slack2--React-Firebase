897be4f9ec4ebd66df741a1729cfd9a28e2219c7
aydevelop <ay.develop1@gmail.com>
2020-08-22 21:32:23 +0300

    Create channels collection

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,33 +1,75 @@
-import React from "react";
-import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
+import React from 'react'
+import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react'
+import firebase from '../../firebase'
 
 class Channels extends React.Component {
   state = {
+    user: this.props.currentUser,
     channels: [],
-    channelName: "",
-    channelDetails: "",
-    modal: false
-  };
+    channelName: '',
+    channelDetails: '',
+    modal: false,
+    channelsRef: firebase.database().ref('channels'),
+  }
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
 
-  openModal = () => this.setState({ modal: true });
+  isValid = ({ channelName, channelDetails }) => {
+    return channelName && channelDetails
+  }
 
-  closeModal = () => this.setState({ modal: false });
+  addChannel = () => {
+    const { channelsRef, channelName, channelDetails } = this.state
+
+    const key = channelsRef.push().key
+
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: this.state.user.displayName,
+        avatar: this.state.user.photoURL,
+      },
+    }
+
+    channelsRef
+      .child(key)
+      .set(newChannel)
+      .then(() => {
+        this.setState({ channelName: '', channelDetails: '' })
+        this.closeModal()
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
+  handleSubmit = (event) => {
+    if (this.isValid(this.state)) {
+      this.addChannel()
+    } else {
+      console.error('Not valid')
+    }
+  }
+
+  openModal = () => this.setState({ modal: true })
+
+  closeModal = () => this.setState({ modal: false })
 
   render() {
-    const { channels, modal } = this.state;
+    const { channels, modal } = this.state
 
     return (
       <React.Fragment>
-        <Menu.Menu style={{ paddingBottom: "2em" }}>
+        <Menu.Menu style={{ paddingBottom: '2em' }}>
           <Menu.Item>
             <span>
-              <Icon name="exchange" /> CHANNELS
-            </span>{" "}
-            ({channels.length}) <Icon name="add" onClick={this.openModal} />
+              <Icon name='exchange' /> CHANNELS
+            </span>{' '}
+            ({channels.length}) <Icon name='add' onClick={this.openModal} />
           </Menu.Item>
           {/* Channels */}
         </Menu.Menu>
@@ -40,8 +82,8 @@ class Channels extends React.Component {
               <Form.Field>
                 <Input
                   fluid
-                  label="Name of Channel"
-                  name="channelName"
+                  label='Name of Channel'
+                  name='channelName'
                   onChange={this.handleChange}
                 />
               </Form.Field>
@@ -49,8 +91,8 @@ class Channels extends React.Component {
               <Form.Field>
                 <Input
                   fluid
-                  label="About the Channel"
-                  name="channelDetails"
+                  label='About the Channel'
+                  name='channelDetails'
                   onChange={this.handleChange}
                 />
               </Form.Field>
@@ -58,17 +100,17 @@ class Channels extends React.Component {
           </Modal.Content>
 
           <Modal.Actions>
-            <Button color="green" inverted>
-              <Icon name="checkmark" /> Add
+            <Button onClick={this.handleSubmit} color='green' inverted>
+              <Icon name='checkmark' /> Add
             </Button>
-            <Button color="red" inverted onClick={this.closeModal}>
-              <Icon name="remove" /> Cancel
+            <Button color='red' inverted onClick={this.closeModal}>
+              <Icon name='remove' /> Cancel
             </Button>
           </Modal.Actions>
         </Modal>
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default Channels;
+export default Channels
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -16,7 +16,7 @@ class SidePanel extends React.Component {
         style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
       >
         <UserPanel />
-        <Channels />
+        <Channels currentUser={currentUser} />
       </Menu>
     )
   }
