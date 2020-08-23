0b6745a2fd80223387b752e4b4f4b78156b47998
aydevelop <ay.develop1@gmail.com>
2020-08-23 20:30:25 +0300

    Display messages, create message component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -4,12 +4,39 @@ import firebase from '../../firebase'
 
 import MessagesHeader from './MessagesHeader'
 import MessageForm from './MessageForm'
+import Message from './Message'
 
 class Messages extends React.Component {
   state = {
     messagesRef: firebase.database().ref('messages'),
     channel: this.props.currentChannel,
     user: this.props.currentUser,
+    messages: [],
+    messagesLoading: true,
+  }
+
+  componentDidMount() {
+    const { channel, messages, messagesLoading, user } = this.state
+    if (!channel || !user) {
+      return
+    }
+
+    const loadedMessages = []
+    this.state.messagesRef.child(channel.id).on('child_added', (snap) => {
+      loadedMessages.push(snap.val())
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false,
+      })
+    })
+  }
+
+  displayMsg = () => {
+    return this.state.messages.map((msg) => {
+      return (
+        <Message key={msg.timestamp} message={msg} user={this.state.user} />
+      )
+    })
   }
 
   render() {
@@ -18,7 +45,9 @@ class Messages extends React.Component {
         <MessagesHeader />
 
         <Segment>
-          <Comment.Group className='messages'>{/* Messages */}</Comment.Group>
+          <Comment.Group className='messages'>
+            {this.displayMsg()}
+          </Comment.Group>
         </Segment>
 
         <MessageForm
