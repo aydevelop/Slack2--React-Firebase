5b09e1650c92dbb5a711a965164d61ee8492080b
aydevelop <ay.develop1@gmail.com>
2020-08-27 16:24:35 +0300

    Emoji picker is added

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -4,6 +4,8 @@ import firebase from '../../firebase'
 import FileModal from './FileModal'
 import uuidv4 from 'uuid.v4'
 import ProgressBar from './ProgressBar'
+import { Picker, emojiIndex } from 'emoji-mart'
+import 'emoji-mart/css/emoji-mart.css'
 
 class MessageForm extends React.Component {
   state = {
@@ -19,6 +21,8 @@ class MessageForm extends React.Component {
     typingRef: firebase.database().ref('typing'),
     errors: [],
     modal: false,
+    emojiPicker: false,
+    messageInputRef: null,
   }
 
   openModal = () => this.setState({ modal: true })
@@ -139,16 +143,52 @@ class MessageForm extends React.Component {
     }
   }
 
+  handleTogglePicker = () => {
+    this.setState({ emojiPicker: !this.state.emojiPicker })
+  }
+
+  handleAddEmoji = (emoji) => {
+    const oldMessage = this.state.message
+    const newMessage = this.colonToUnicode(` ${oldMessage} ${emoji.colons} `)
+    this.setState({ message: newMessage, emojiPicker: false })
+    setTimeout(() => this.messageInputRef.focus(), 0)
+  }
+
+  colonToUnicode = (message) => {
+    return message.replace(/:[A-Za-z0-9_+-]+:/g, (x) => {
+      x = x.replace(/:/g, '')
+      let emoji = emojiIndex.emojis[x]
+      if (typeof emoji !== 'undefined') {
+        let unicode = emoji.native
+        if (typeof unicode !== 'undefined') {
+          return unicode
+        }
+      }
+      x = ':' + x + ':'
+      return x
+    })
+  }
+
   render() {
     return (
       <Segment className='message__form'>
+        {this.state.emojiPicker && (
+          <Picker
+            set='apple'
+            onSelect={this.handleAddEmoji}
+            className='emojipicker'
+            title='Pick your emoji'
+            emoji='point_up'
+          />
+        )}
         <Input
+          ref={(node) => (this.messageInputRef = node)}
           onChange={this.handleChange}
           onKeyDown={this.handleKeyDown}
           fluid
           name='message'
           style={{ marginBottom: '0.7em' }}
-          label={<Button icon={'add'} />}
+          label={<Button icon={'add'} onClick={this.handleTogglePicker} />}
           labelPosition='left'
           placeholder='Write your message'
           value={this.state.message}
