a2902241c54e4ee5e6d661459814e9260072bf41
aydevelop <ay.develop1@gmail.com>
2020-08-27 17:53:47 +0300

    Firestore security rules

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -25,6 +25,13 @@ class MessageForm extends React.Component {
     messageInputRef: null,
   }
 
+  componentWillUnmount() {
+    if (this.state.uploadTask !== null) {
+      this.state.uploadTask.cencel()
+      this.setState({ uploadTask: null })
+    }
+  }
+
   openModal = () => this.setState({ modal: true })
   closeModal = () => this.setState({ modal: false })
 
@@ -129,7 +136,7 @@ class MessageForm extends React.Component {
 
   getPath = () => {
     if (this.props.isPrivateChannel) {
-      return `chat/private-${this.state.channel.id}`
+      return `chat/private/${this.state.channel.id}`
     } else {
       return 'chat/public'
     }
